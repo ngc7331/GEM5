7a5b6af5263714131c336d90d28cad49d352c1b1
ngc7331 <ngc7331@outlook.com>
2024-02-21 12:01:18 +0800

    chore: add script to run xs-gem5 in docker

---
+++ run.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+GEM5_HOME=$(pwd)
+GEM5=$GEM5_HOME/build/RISCV/gem5.opt
+CONFIG=$GEM5_HOME/configs/example/fs.py
+
+BIN=${BIN:-coremark-1000}
+BIN_FILE=${GEM5_HOME}/../NutShell/ready-to-run/${BIN}.bin
+
+RUN_CMD="$GEM5 $CONFIG \
+    --xiangshan-system --cpu-type=DerivO3CPU \
+    --mem-size=8GB \
+    --caches --cacheline_size=64 \
+    --l1i_size=64kB --l1i_assoc=8 \
+    --l1d_size=64kB --l1d_assoc=8 \
+    --l1d-hwp-type=XSCompositePrefetcher \
+    --short-stride-thres=0 \
+    --l2cache --l2_size=1MB --l2_assoc=8 \
+    --l3cache --l3_size=16MB --l3_assoc=16 \
+    --l1-to-l2-pf-hint \
+    --l2-hwp-type=WorkerPrefetcher \
+    --l2-to-l3-pf-hint \
+    --l3-hwp-type=WorkerPrefetcher \
+    --bp-type=DecoupledBPUWithFTB --enable-loop-predictor \
+    --generic-rv-cpt=${BIN_FILE} --raw-cpt \
+"
+
+./util/docker-wrapper.sh "${RUN_CMD}"
